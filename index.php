b8c731d0f6166b7861e4d81224962718921ce265
MRX007 <mrx31995@gmail.com>
2025-01-07 23:14:29 +0500

    my first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$kw = '';
+// Keywords for SEO
+$desc = '';
+// Description for SEO
+$title = 'Главная';
+$page = 'index';
+
+include 'partials/header.php' ?>
+
+<main class='main' style='margin-top: 0px !important; height: 100vh;'>
+    <!-- <svg>
+        <use xlink:href='/assets/icons/sprite.svg#arrow-header'></use>
+    </svg> -->
+
+</main>
+
+<?php include 'partials/footer.php' ?>
---
+++ partials/footer.php
@@ -0,0 +1,149 @@
+   
+        <footer class="footer">
+            <ul class="footer__nav">
+                <li>
+                    <a href="/about.php" class="footer__nav-link">
+                        о компании
+                        <svg>
+                            <use xlink:href="/assets/icons/sprite.svg#arrow-header"></use>
+                        </svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="/services.php" class="footer__nav-link">
+                        услуги
+                        <svg>
+                            <use xlink:href="/assets/icons/sprite.svg#arrow-header"></use>
+                        </svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="/news.php" class="footer__nav-link">
+                        новости
+                        <svg>
+                            <use xlink:href="/assets/icons/sprite.svg#arrow-header"></use>
+                        </svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="/vacancies.php" class="footer__nav-link">
+                        вакансии
+                        <svg>
+                            <use xlink:href="/assets/icons/sprite.svg#arrow-header"></use>
+                        </svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="/partners.php" class="footer__nav-link">
+                        партнеры
+                        <svg>
+                            <use xlink:href="/assets/icons/sprite.svg#arrow-header"></use>
+                        </svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="/contacts.php" class="footer__nav-link">
+                        контакты
+                        <svg>
+                            <use xlink:href="/assets/icons/sprite.svg#arrow-header"></use>
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+
+            <div class="footer__contacts">
+                <div class="footer__contacts-text">
+                    Контактный телефон
+                </div>
+                <a href="tel:+998712312424" class="footer__contacts-tel">
+                    +998 71 231 24 24
+                </a>
+                <div class="footer__contacts__social">
+                    <a href="" class="footer__contacts__social-link">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                            <g clip-path="url(#clip0_1154_708)">
+                                <path d="M3.63889 7.63168L5.25078 11.6611L7.34925 9.56264L10.9472 12.4224L13.4805 1.03174L0.520508 6.43002L3.63889 7.63168ZM9.77717 4.29982L5.81033 7.91802L5.31624 9.78017L4.4035 7.4979L9.77717 4.29982Z" fill="#696969" />
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_1154_708">
+                                    <rect width="12.96" height="12.96" fill="white" transform="translate(0.520508 0.24707)" />
+                                </clipPath>
+                            </defs>
+                        </svg>
+                    </a>
+                    <a href="" class="footer__contacts__social-link">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                            <g clip-path="url(#clip0_1154_695)">
+                                <path d="M11.5821 0.24707H2.41895C1.37223 0.24707 0.520508 1.0988 0.520508 2.14551V11.3086C0.520508 12.3553 1.37223 13.2071 2.41895 13.2071H11.5821C12.6288 13.2071 13.4805 12.3553 13.4805 11.3086V2.14551C13.4805 1.0988 12.6288 0.24707 11.5821 0.24707ZM7.02582 10.1189C5.14152 10.1189 3.60863 8.58606 3.60863 6.70176C3.60863 4.81746 5.14152 3.28457 7.02582 3.28457C8.91012 3.28457 10.443 4.81746 10.443 6.70176C10.443 8.58606 8.91012 10.1189 7.02582 10.1189ZM10.8227 4.04395C10.1946 4.04395 9.68363 3.53295 9.68363 2.90488C9.68363 2.27682 10.1946 1.76582 10.8227 1.76582C11.4508 1.76582 11.9618 2.27682 11.9618 2.90488C11.9618 3.53295 11.4508 4.04395 10.8227 4.04395Z" fill="#696969" />
+                                <path d="M10.823 2.52515C10.6135 2.52515 10.4434 2.69531 10.4434 2.90483C10.4434 3.11435 10.6135 3.28452 10.823 3.28452C11.0326 3.28452 11.2027 3.11435 11.2027 2.90483C11.2027 2.69531 11.0326 2.52515 10.823 2.52515Z" fill="#696969" />
+                                <path d="M7.02598 4.04395C5.56062 4.04395 4.36816 5.2364 4.36816 6.70176C4.36816 8.16711 5.56062 9.35957 7.02598 9.35957C8.49133 9.35957 9.68379 8.16711 9.68379 6.70176C9.68379 5.2364 8.49133 4.04395 7.02598 4.04395Z" fill="#696969" />
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_1154_695">
+                                    <rect width="12.96" height="12.96" fill="white" transform="translate(0.520508 0.24707)" />
+                                </clipPath>
+                            </defs>
+                        </svg>
+                    </a>
+                    <a href="" class="footer__contacts__social-link">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                            <g clip-path="url(#clip0_1154_702)">
+                                <path d="M8.32976 13.2071V7.30502H10.2489L10.5342 4.99418H8.32976V3.52227C8.32976 2.85544 8.50883 2.39889 9.43127 2.39889H10.6001V0.338647C10.0314 0.275414 9.45976 0.244883 8.8878 0.247192C7.19145 0.247192 6.02678 1.3216 6.02678 3.29399V4.98986H4.12012V7.3007H6.03094V13.2071H8.32976Z" fill="#696969" />
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_1154_702">
+                                    <rect width="12.96" height="12.96" fill="white" transform="translate(0.520508 0.24707)" />
+                                </clipPath>
+                            </defs>
+                        </svg>
+                    </a>
+                </div>
+                <a href="https://kirano.uz/" class="made__in-kirano">Разработано в Кирано</a>
+            </div>
+        </footer>
+
+
+    </div>
+
+
+    <script>
+        let lang = document.querySelector(".lang");
+        const handleLang = () => {
+            lang.classList.toggle("active");
+        };
+
+        let mobileMenu = document.querySelector(".header__wrapper__nav");
+        let burger = document.querySelector(".burger");
+        const headerWrapper = document.querySelector('.header__wrapper__nav');
+
+        headerWrapper.addEventListener('click', function(event) {
+            if (event.target == headerWrapper) {
+                toggleMobileMenu()
+            }
+        })
+
+
+        const toggleMobileMenu = () => {
+            mobileMenu.classList.toggle("active");
+            burger.classList.toggle("active");
+            document.body.classList.toggle("no-scroll");
+        };
+    </script>
+    <!-- /.wrapper -->
+    <script src="/assets/libraries/jquery/jquery.min.js"></script>
+    <script src="/assets/libraries/swiper/swiper-bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/inputmask/4.0.8/jquery.inputmask.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/16.0.0/js/intlTelInput.min.js"></script>
+    <script src="/assets/libraries/phonemask/phonemask.min.js"></script>
+    <script src="/assets/js/modal.js"></script>
+    <script src="/assets/js/swipers.js"></script>
+    <script src="/assets/js/script.js"></script>
+    <script src="/assets/js/lazyImage.js"></script>
+    <script src="/assets/js/telInput.js"></script>
+    <script src="/assets/js/index.js"></script>
+
+
+    </body>
+
+    </html>
